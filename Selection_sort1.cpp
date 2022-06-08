a4d60325e242b13808f2d45d4fec70ef71f43946
Zaid Shaikh <80575794+Zaid5775@users.noreply.github.com>
2022-06-08 15:31:21 +0530

    Add files via upload

---
+++ Selection_sort1.cpp
@@ -0,0 +1,13 @@
+void selectionSort(vector<int>& arr, int n)
+{   
+    for(int i=0; i<n; i++){
+        int minindex = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j]<arr[minindex]){
+                minindex =j;
+            }
+            
+        }
+        swap(arr[minindex],arr[i]);
+    }
+}
